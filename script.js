9227094b360ae50e84fb9ee96069af4489925ce9
satyajeetdc <satyajeetdc@gmail.com>
2024-03-28 15:05:11 +0530

    message

---
+++ script.js
@@ -0,0 +1,45 @@
+function navAnimation() {
+  let nav = document.querySelector("nav");
+
+  nav.addEventListener("mouseenter", function () {
+    let tl = gsap.timeline();
+    tl.to("#nav-bottom", {
+      height: "23vh",
+    });
+    tl.to(".nav-part2 h5", {
+      display: "block",
+    });
+    tl.to(".nav-part2 h5 span", {
+      y: 0,
+      stagger: {
+        amount: 0.6,
+      },
+    });
+  });
+
+  nav.addEventListener("mouseleave", function () {
+    let tl = gsap.timeline();
+    tl.to(".nav-part2 h5 span", {
+    //   transform: `translateY(25px)`,
+      y: 25,
+      stagger: {
+        amount: 0.2,
+      },
+    });
+    tl.to(".nav-part2 h5", {
+      display: "none",
+      duration: 0.1,
+    });
+    tl.to("#nav-bottom", {
+      height: 0,
+      duration: 0.2,
+    });
+  });
+}
+
+
+
+
+
+
+navAnimation();
