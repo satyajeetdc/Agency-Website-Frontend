3386bf2dbade6386aa1e2419372cf93b599b8085
satyajeetdc <satyajeetdc@gmail.com>
2024-03-29 17:49:33 +0530

    29mar17:49

--- script.js
+++ script.js
@@ -1,3 +1,68 @@
+function locomotiveAnimation() {
+  gsap.registerPlugin(ScrollTrigger);
+
+  // Using Locomotive Scroll from Locomotive https://github.com/locomotivemtl/locomotive-scroll
+
+  const locoScroll = new LocomotiveScroll({
+    el: document.querySelector("#main"),
+    smooth: true,
+  });
+  // each time Locomotive Scroll updates, tell ScrollTrigger to update too (sync positioning)
+  locoScroll.on("scroll", ScrollTrigger.update);
+
+  // tell ScrollTrigger to use these proxy methods for the "#main" element since Locomotive Scroll is hijacking things
+  ScrollTrigger.scrollerProxy("#main", {
+    scrollTop(value) {
+      return arguments.length
+        ? locoScroll.scrollTo(value, 0, 0)
+        : locoScroll.scroll.instance.scroll.y;
+    }, // we don't have to define a scrollLeft because we're only scrolling vertically.
+    getBoundingClientRect() {
+      return {
+        top: 0,
+        left: 0,
+        width: window.innerWidth,
+        height: window.innerHeight,
+      };
+    },
+    // LocomotiveScroll handles things completely differently on mobile devices - it doesn't even transform the container at all! So to get the correct behavior and avoid jitters, we should pin things with position: fixed on mobile. We sense it by checking to see if there's a transform applied to the container (the LocomotiveScroll-controlled element).
+    pinType: document.querySelector("#main").style.transform
+      ? "transform"
+      : "fixed",
+  });
+
+  // each time the window updates, we should refresh ScrollTrigger and then update LocomotiveScroll.
+  ScrollTrigger.addEventListener("refresh", () => locoScroll.update());
+
+  // after everything is set up, refresh() ScrollTrigger and update LocomotiveScroll because padding may have been added for pinning, etc.
+  ScrollTrigger.refresh();
+}
+
+function loadingAnimation() {
+  let tl = gsap.timeline();
+tl.from("#page1", {
+  opacity:0,
+  duration:0.2,
+  delay:0.2 
+})
+tl.from("#page1", {
+  transform:"scaleX(0.7) scaleY(0.2) translateY(80%)",
+  borderradius:"200px",
+  duration:2,
+  ease:"expo.out"
+})
+tl.from("nav", {
+  opacity:0,
+  delay:-0.2
+})
+
+tl.from("#page1 h1, #page1 p, #page1 div", {
+  opacity:0,
+  duration:0.5,
+  stagger:0.2
+})
+}
+
 function navAnimation() {
   let nav = document.querySelector("nav");
 
@@ -85,19 +150,70 @@ function videoAnimation() {
   });
 }
 
-let sections = document.querySelectorAll(".sec-right");
-sections.forEach(function(elem) {
-  elem.addEventListener("mouseenter", function() {
+function videoAnimation1() {
+  let sections = document.querySelectorAll(".sec-right");
+  sections.forEach(function (elem) {
+    elem.addEventListener("mouseenter", function () {
       elem.childNodes[3].style.opacity = 1;
       elem.childNodes[3].play();
+    });
+    elem.addEventListener("mouseleave", function () {
+      elem.childNodes[3].style.opacity = 0;
+      elem.childNodes[3].load();
+    });
   });
-  elem.addEventListener("mouseleave", function() {
-    elem.childNodes[3].style.opacity = 0;
-    elem.childNodes[3].load();
-})
-})
+}
+
+function slidingList() {
+  gsap.from("#bottom-part2 h4", {
+    x: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#bottom-part2",
+      scroller: "#main",
+      markers: false,
+      start: "top 80%",
+      end: "top 10%",
+      scrub: true,
+    },
+  });
+
+  gsap.from("#bottom-part3 h4", {
+    x: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#bottom-part3",
+      scroller: "#main",
+      markers: false,
+      start: "top 80%",
+      end: "top 10%",
+      scrub: true,
+    },
+  });
+
+  gsap.from("#bottom-part4 h4", {
+    x: 0,
+    duration: 1,
+    scrollTrigger: {
+      trigger: "#bottom-part4",
+      scroller: "#main",
+      markers: false,
+      start: "top 80%",
+      end: "top 10%",
+      scrub: true,
+    },
+  });
+}
 
 
-// navAnimation();
-// blobAnimation();
-// videoAnimation(); 
+
+
+
+
+locomotiveAnimation();
+loadingAnimation();
+navAnimation();
+blobAnimation();
+videoAnimation();
+videoAnimation1();
+slidingList();
