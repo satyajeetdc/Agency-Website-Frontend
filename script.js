68d30f99993753f89fd4e1945e23b965977838e0
satyajeetdc <satyajeetdc@gmail.com>
2024-03-29 12:00:32 +0530

    29mar12:00

--- script.js
+++ script.js
@@ -85,6 +85,19 @@ function videoAnimation() {
   });
 }
 
+let sections = document.querySelectorAll(".sec-right");
+sections.forEach(function(elem) {
+  elem.addEventListener("mouseenter", function() {
+      elem.childNodes[3].style.opacity = 1;
+      elem.childNodes[3].play();
+  });
+  elem.addEventListener("mouseleave", function() {
+    elem.childNodes[3].style.opacity = 0;
+    elem.childNodes[3].load();
+})
+})
+
+
 // navAnimation();
 // blobAnimation();
 // videoAnimation(); 
